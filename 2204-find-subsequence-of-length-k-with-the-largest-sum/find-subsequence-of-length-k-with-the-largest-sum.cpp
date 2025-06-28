4798547cede176da64af904202f83c909689a0cf
Siamheil <harshitvishwa826@gmail.com>
2025-06-28 22:17:12 +0530

    Time: 3 ms (42.81%) | Memory: 13.7 MB (78.54%) - LeetSync

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
+    typedef pair<int,int> P;
     vector<int> maxSubsequence(vector<int>& nums, int k) {
-        vector<pair<int, int>> numsWithIndices;
-        for (int i = 0; i < nums.size(); i++) {
-            numsWithIndices.push_back({nums[i], i});
+        priority_queue<P, vector<P> , greater<P> > pq;
+        for(int i =0; i< nums.size(); i++)
+        {
+            pq.push({nums[i], i});
+            if(pq.size() > k)
+            pq.pop();
         }
-
-        // Sort by value descending
-        sort(numsWithIndices.begin(), numsWithIndices.end(),
-             [](const pair<int, int>& a, const pair<int, int>& b) {
-                 return a.first > b.first;
-             });
-
-        // Take top k and sort by original index
-        vector<pair<int, int>> topK(numsWithIndices.begin(), numsWithIndices.begin() + k);
-        sort(topK.begin(), topK.end(),
-             [](const pair<int, int>& a, const pair<int, int>& b) {
-                 return a.second < b.second;
-             });
-
-        // Extract values
-        vector<int> result;
-        for (auto& p : topK) {
-            result.push_back(p.first);
-        }
-
-        return result;
+        vector<int> answer;
+        while(!pq.empty())
+         {
+            answer.push_back(pq.top().second);
+            pq.pop();
+         }
+        sort(answer.begin(), answer.end());
+        for(int i = 0; i< answer.size(); i++)
+            answer[i] = nums[answer[i]];
+        
+        return answer;
     }
 };
