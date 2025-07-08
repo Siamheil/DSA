e4fe0ebcd36c180c34bc901679abcbede16ad704
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-07-08 12:23:17 +0530

    Time: 14 ms (22.33%) | Memory: 7.7 MB (87.57%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,20 +1,9 @@
 class Solution {
 public:
     int fib(int n) {
-        // Initialize the first two fibonacci numbers
-        int previous = 0, current = 1;
-      
-        // Loop to calculate the nth fibonacci number
-        while (n--) {
-            // Calculate the next fibonacci number
-            int next = previous + current;
-          
-            // Update the previous and current values for the next iteration
-            previous = current;
-            current = next;
+        if(n==0 || n==1){
+            return n;
         }
-      
-        // Return the nth fibonacci number which is now stored in 'previous'
-        return previous;
+        return fib(n-1) + fib(n-2);
     }
 };
