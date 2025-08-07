a486348a94e386d638c6f48634df3a52986f583c
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-08-07 22:23:50 +0530

    InsertionInLinkedList.cpp

--- InsertionInLinkedList.cpp
+++ InsertionInLinkedList.cpp
@@ -17,6 +17,32 @@ void insertAthead(Node* &head,int val){
 	new_node->next = head;
 	head = new_node;
 }
+void insertAttail(Node* &head,int val){
+	
+	Node* new_node = new Node(val);
+	Node* temp = head;
+	while(temp->next!=NULL){
+		temp = temp->next;
+	}
+	temp->next = new_node;
+}
+
+void insertAtPosition(Node* &head,int val,int position){
+	if(position==0){
+		insertAthead(head,val);
+		return;
+	}
+	Node* new_node = new Node(val);
+	Node* temp = head;
+	int current_pos = 0;
+	while(current_pos!=position-1){
+		temp=temp->next;
+		current_pos++;
+	}
+	
+	new_node->next=temp->next;
+	temp->next=new_node;
+}
 
 void display(Node* head){
 	Node* temp = head;
@@ -33,6 +59,10 @@ int main(){
 	insertAthead(head,1);
 	display(head);
 	insertAthead(head,12);
+	display(head);
+	insertAttail(head,22);
+	display(head);
+	insertAtPosition(head,4,1);
 	display(head);
 
 }
