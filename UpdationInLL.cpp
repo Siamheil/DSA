8749d348d339d4aaf8b9975cf52d6ab03323947d
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-08-08 06:57:53 +0530

    UpdationInLL.cpp

--- UpdationInLL.cpp
+++ UpdationInLL.cpp
@@ -51,6 +51,38 @@ void update(Node* &head,int k,int val){
 	temp->val=val;
 }
 
+void deleteAtHead(Node *&head){
+	Node* temp=head;
+	head=head->next;
+	free(temp);
+}
+void deleteAtTail(Node* &head){
+	Node* second_last=head;
+	while(second_last->next->next!=NULL){
+		second_last=second_last->next}
+
+	Node* temp=second_last->next;
+	second_last->next=NULL;
+	free(temp);
+}
+
+void deleteAtPosition(Node* &head,int pos){
+	if(pos==0){
+		deleteAtHead(head);
+		return;
+	}
+	int curr_pos=0;
+	Node *prev=head;
+	while(curr_pos!=pos-1){
+		pre=prev->next;
+		curr_pos++;
+	}
+	Node* temp=prev->next;
+	prev->next=prev->next->next;
+	free(temp);
+}
+
+
 void display(Node* head){
 	Node* temp = head;
 	while(temp!=NULL){
@@ -60,18 +92,3 @@ void display(Node* head){
 	cout<<"NULL"<<endl;
 }
 
-int main(){
-	
-	Node* head = NULL;
-	insertAthead(head,1);
-	display(head);
-	insertAthead(head,12);
-	display(head);
-	insertAttail(head,22);
-	display(head);
-	insertAtPosition(head,4,1);
-	display(head);
-	update(head,2,5);
-	display(head);
-
-}
