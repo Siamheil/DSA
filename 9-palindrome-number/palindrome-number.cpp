8342cfc88247c8a60d1b6941e13c1779b06b8880
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-08-15 12:15:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (67.58%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-//palindrome
     bool isPalindrome(int x) {
-        if (x < 0 || (x && x % 10 == 0)) {
-            return false;
-        }
-        int y = 0;
-        for (; y < x; x /= 10) {
-            y = y * 10 + x % 10;
-        }
-        return x == y || x == y / 10;
+       long long rev=0,y=x;
+       if(x<0) return false;
+       while(x!=0)
+       {
+        rev*=10;
+        rev+=(x%10);
+        x/=10;
+       } 
+       if(y==rev) return 1;
+       else return 0;
     }
 };
