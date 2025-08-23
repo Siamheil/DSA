5fb486211027c6e253523eed58bb46cecbeb2f29
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-08-23 21:57:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (33.29%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        stack<char>st;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
-                st.push(s[i]);
-            }
-            else{
-                if(st.empty())
-                return 0;
-                else if(s[i]==')')
-                {
-                    if(st.top()!='(')
-                    return 0;
-                    else
-                    st.pop();
-                }
-                 else if(s[i]=='}')
-                {
-                    if(st.top()!='{')
-                    return 0;
-                    else
-                    st.pop();
-                }
-                 else
-                {
-                    if(st.top()!='[')
-                    return 0;
-                    else
-                    st.pop();
-                }
+    bool isValid(string& k) {
+        stack<char> st;
+        int n = k.size();
+
+        for (int i = 0; i < n; i++) {
+            // If opening bracket, push to stack
+            if (k[i] == '(' || k[i] == '{' || k[i] == '[') {
+                st.push(k[i]);
             } 
+            else {
+                // If stack is empty, no matching opening bracket
+                if (st.empty()) return false;
+
+                // Check for matching pair
+                if ((st.top() == '(' && k[i] == ')') ||
+                    (st.top() == '{' && k[i] == '}') ||
+                    (st.top() == '[' && k[i] == ']')) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
         }
+
+        // If stack is empty, all brackets matched
         return st.empty();
     }
 };
