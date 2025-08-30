35c0ead99de14f6c1a1ecfadcbce2f1671dafa2b
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-08-30 22:58:13 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (33.18%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     int search(vector<int>& arr, int target) {
-        int start=0,end=arr.size()-1,mid;
-        while(start<=end)
-        {
-            mid=start+(end-start)/2;
-            //find my element
-            if(arr[mid]==target)
-            return mid;
-            //left side sorted
-            else if(arr[mid]>=arr[0])
-            {
-                if(arr[start]<=target && arr[mid]>=target)
-                end=mid-1;
-                else
-                start=mid+1;
+        int n=arr.size();
+        int start=0,end=n-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target) return mid;
+
+            // left sorted
+            if(arr[start]<=arr[mid]){
+                if(arr[start]<=target && target<arr[mid]){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
+                }
             }
-            //right side sorted
-            else
-            {
-                if(arr[mid]<target && arr[end]>=target)
-                start=mid+1;
-                else
-                end=mid-1;
+            // right sorted
+            else {
+                if(arr[mid]<target && target<=arr[end]){
+                    start=mid+1;
+                }else{
+                    end=mid-1;
+                }
             }
         }
         return -1;
