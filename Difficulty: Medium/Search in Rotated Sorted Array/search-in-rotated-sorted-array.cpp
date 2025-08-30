09849e4763b98c125ba539ebb6b128919436a8d6
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-08-30 23:02:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
@@ -1,28 +1,28 @@
 class Solution {
-public:
-    int search(vector<int>& arr, int target) {
-        int start=0,end=arr.size()-1,mid;
-        while(start<=end)
-        {
-            mid=start+(end-start)/2;
-            //find my element
-            if(arr[mid]==target)
-            return mid;
-            //left side sorted
-            else if(arr[mid]>=arr[0])
-            {
-                if(arr[start]<=target && arr[mid]>=target)
-                end=mid-1;
-                else
-                start=mid+1;
+  public:
+    int search(vector<int>& arr, int key) {
+        // Code Here
+        int n=arr.size();
+        int start=0,end=n-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==key) return mid;
+            
+            //left sorted
+            if(arr[start]<=arr[mid]){
+                if(arr[start]<=key && key<=arr[mid]){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
+                }
             }
-            //right side sorted
-            else
-            {
-                if(arr[mid]<target && arr[end]>=target)
-                start=mid+1;
-                else
-                end=mid-1;
+            //right sorted
+            else{
+                if(arr[mid]<=key && key<=arr[end]){
+                    start=mid+1;
+                }else{
+                    end=mid-1;
+                }
             }
         }
         return -1;
