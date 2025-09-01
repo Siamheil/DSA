6ff79b07476f5527d529d05fbee0e5d933e5e048
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-09-01 13:46:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (78.34%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int sum_s=0;
+        int XOR=0;
         for(char &ch : s){
-            sum_s += ch;
+            XOR ^= ch;
         }
-        int sum_t=0;
         for(char &ch : t){
-            sum_t += ch;
+            XOR ^= ch;
         }
-        return (char)(sum_t - sum_s);
+        return (char)XOR;
     }
 };
