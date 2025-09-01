67501e9b3cffd098e8c3dc46f4a7b15741980255
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-09-01 12:31:10 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (32.10%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        int n = numbers.size();
-        int start = 0, end = n - 1;
-
-        while (start < end) {
-            int sum = numbers[start] + numbers[end];
-            if (sum == target) {
-                // return 1-based index as per problem statement
-                return {start + 1, end + 1};
-            } else if (sum < target) {
-                start++;
-            } else {
-                end--;
-            }
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        int start=0,end=n-1;
+        while(start<end){
+            int sum=nums[start]+nums[end];
+            if(sum==target)
+            return {start+1,end+1};
+            else if(sum<target)
+            start++;
+            else
+            end--;
         }
-        return {}; // in case no pair is found
+        return {};
     }
 };
