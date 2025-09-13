1ab23b8c9314e13748334dea3de4821bc3ca6d66
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-09-13 20:42:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.cpp
+++ Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.cpp
@@ -1,20 +1,20 @@
 class Solution {
   public:
     void pushZerosToEnd(vector<int>& arr) {
-        // code here
-        int n=arr.size();
-        int j=-1;
-        for(int i=0;i<n;i++){
-            if(arr[i]==0){
-                j=i;
+        int n = arr.size();
+        int j = -1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
+                j = i;
                 break;
             }
         }
-        if(j==-1) return;
-        for(int i=j+1;i<n;i++){
-            if(arr[i]!=0){
-                swap(arr[i],arr[j]);
-                j++;
+
+        if (j == -1) return; 
+        for (int i = j + 1; i < n; i++) {
+            if (arr[i] != 0) {
+                swap(arr[i], arr[j]);
+                j++; 
             }
         }
     }
