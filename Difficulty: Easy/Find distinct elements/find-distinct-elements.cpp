1615d45e1df374ace26ef3e8031304f9c2f91979
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-09-15 22:06:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find distinct elements/find-distinct-elements.cpp
@@ -0,0 +1,15 @@
+// User function Template for C++
+int distinct(vector<int>& arr) {
+    int n = arr.size();
+    if (n == 0) return 0;
+
+    sort(arr.begin(), arr.end());
+
+    int count = 1; 
+    for (int i = 1; i < n; i++) {
+        if (arr[i] != arr[i - 1]) {
+            count++;
+        }
+    }
+    return count;
+}
