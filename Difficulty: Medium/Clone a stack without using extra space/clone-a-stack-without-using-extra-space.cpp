eb7973b81183828565cc09a135def08050da2e4a
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-09-20 22:07:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Clone a stack without using extra space/clone-a-stack-without-using-extra-space.cpp
@@ -0,0 +1,17 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    void clonestack(stack<int> st, stack<int>& cloned) {
+        // code here
+        stack<int>temp;
+        while(!st.empty()){
+            temp.push(st.top());
+            st.pop();
+        }
+        while(!temp.empty()){
+            cloned.push(temp.top());
+            temp.pop();
+        }
+    }
+};
