e4a2ae680cf2e33e642642217c0f91353b4f42c8
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-09-21 13:38:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Previous Smaller Element/previous-smaller-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    vector<int> prevSmaller(vector<int>& arr) {
+        //  code here
+        int n=arr.size();
+        reverse(arr.begin(),arr.end());
+        vector<int>ans(n,-1);
+        stack<int>st;
+        st.push(0);
+        for(int i=1;i<n;i++){
+            while(!st.empty() && arr[i]<arr[st.top()]){
+                ans[st.top()]=arr[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
