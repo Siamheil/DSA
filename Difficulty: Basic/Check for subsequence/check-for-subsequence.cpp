41fca34385b3e5a63fa53f9257cedff68803d7fa
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-10-12 14:37:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check for subsequence/check-for-subsequence.cpp
@@ -0,0 +1,15 @@
+
+class Solution {
+  public:
+    bool isSubSequence(string A, string B) {
+        // code here
+        int i=0,j=0;
+        while(i<A.size() && j<B.size()){
+            if(A[i]==B[j]){
+                i++;
+            }
+            j++;
+        }
+        return i==A.size();
+    }
+};
