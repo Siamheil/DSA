fad5e05b090624c62ea593e9e1c3f7659e057f69
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-10-18 11:34:18 +0530

    Time: 0 ms (100%), Space: 31.4 MB (16.58%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -11,15 +11,8 @@
  */
 class Solution {
 public:
-    void helper(TreeNode* root,int& count){
-        if(root==NULL) return;
-        count++;
-        helper(root->left,count);
-        helper(root->right,count);
-    }
     int countNodes(TreeNode* root) {
-        int count=0;
-        helper(root,count);
-        return count;
+        if(root==NULL) return 0;
+        return 1+countNodes(root->left)+countNodes(+root->right);
     }
 };
