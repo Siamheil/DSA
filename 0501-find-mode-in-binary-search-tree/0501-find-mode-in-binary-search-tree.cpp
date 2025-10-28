b91765de610e6671173030a8ae49b55da1c94e9e
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-10-28 10:34:58 +0530

    Time: 5 ms (29.88%), Space: 25.5 MB (51.64%) - LeetHub

---
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    unordered_map<int,int>mp;
+    void dfs(TreeNode* root){
+        if(!root) return;
+        dfs(root->left);
+        mp[root->val]++;
+        dfs(root->right);
+    }
+    vector<int> findMode(TreeNode* root) {
+        dfs(root);
+        vector<int>res;
+        int maxFreq=0;
+        for(auto &it:mp){
+            if(it.second>maxFreq){
+                maxFreq=it.second;
+                res={};
+                res.push_back(it.first);
+            }
+            else if(it.second==maxFreq){
+                res.push_back(it.first);
+            }
+        }
+        return res;
+    }
+};
