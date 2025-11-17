bba52bd509d2326bcf5478aa5538c08c3bf11fcc
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-11-17 07:45:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Assign Cookies/assign-cookies.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int maxChildren(vector<int> &greed, vector<int> &cookie) {
+        sort(greed.begin(),greed.end());
+        sort(cookie.begin(),cookie.end());
+        int i=0,j=0;
+        while(i<greed.size()&&j<cookie.size()){
+            if(greed[i]<=cookie[j]) i++;
+            j++;
+        }
+        return i;
+    }
+};
