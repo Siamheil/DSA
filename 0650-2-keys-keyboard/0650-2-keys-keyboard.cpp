300ce54d63041a79516986f1e0ccce9771bbde78
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2026-01-02 11:39:00 +0530

    Time: 14 ms (36.07%), Space: 12.3 MB (22.78%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
+    int dp[1001][1001];
     int solve(int currA,int clipA,int n){
         if(currA==n) return 0;
         if(currA>n) return 100000;
+        if(dp[currA][clipA]!=-1) return dp[currA][clipA];
         int copyAllpaste=1+1+solve(currA+currA,currA,n);
         int paste=1+solve(currA+clipA,clipA,n);
-        return min(copyAllpaste,paste);
+        return dp[currA][clipA]=min(copyAllpaste,paste);
     }
     int minSteps(int n) {
+        memset(dp,-1,sizeof(dp));
         if(n==1) return 0;
         return 1+solve(1,1,n);
     }
